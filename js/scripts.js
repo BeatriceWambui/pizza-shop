5ff373c40d543df903949765b7cab40a995e1162
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-07-29 14:54:16 +0300

    make changes on js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,170 +1,170 @@
-$(document).ready(function(){
-    $("form#orderform").submit(function(event){
-        var name =$("input#name").val();
-        var email =$("input#email").val();
-        if ($("input#name").val() && $("input#email").val() && $("input#house").val() &&("input#area").val()&& $("input#flavor").val() !="" ){
-        alert ("Dear " + name+ " " + email + ". "+ "We are graced by your presence " + "We have received your order we are going to deliver your pizza in a few minutes." );
-        }else{
-            alert("Please enter convinient data.");
-        }
-        event.preventDefault();
+// $(document).ready(function(){
+//     $("form#orderform").submit(function(event){
+//         var name =$("input#name").val();
+//         var email =$("input#email").val();
+//         if ($("input#name").val() && $("input#email").val() && $("input#house").val() &&("input#area").val()&& $("input#flavor").val() !="" ){
+//         alert ("Dear " + name+ " " + email + ". "+ "We are graced by your presence " + "We have received your order we are going to deliver your pizza in a few minutes." );
+//         }else{
+//             alert("Please enter convinient data.");
+//         }
+//         event.preventDefault();
 
-    });
-});
+//     });
+// });
 
-var form = document.forms["orderform"]
+// // var form = document.forms["orderform"]
 
-    var pizzaPrice = new Array();
-        pizzaPrice["none"]=0;
-        pizzaPrice["regular"]=500;
-        pizzaPrice["meduim"]=700;
-        pizzaPrice["mega"]=1000;
+// //     var pizzaPrice = new Array();
+// //         pizzaPrice["none"]=0;
+// //         pizzaPrice["regular"]=500;
+// //         pizzaPrice["meduim"]=700;
+// //         pizzaPrice["mega"]=1000;
 
-function getFlavor(){
-    form=document.forms["orderform"];
-    MyPizzaPrice = 0;
-    myFlavor=document.getElementById("flavor").selectedIndex.value(); 
-    // alert(myFlavor);
-    MyPizzaPrice=pizzaPrice[myFlavor.value];
-    return MyPizzaPrice;
-};
+// // function getFlavor(){
+// //     form=document.forms["orderform"];
+// //     MyPizzaPrice = 0;
+// //     myFlavor=document.getElementById("flavor").selectedIndex.value(); 
+// //     // alert(myFlavor);
+// //     MyPizzaPrice=pizzaPrice[myFlavor.value];
+// //     return MyPizzaPrice;
+// // };
 
-function originalThinCrust(){
-    form=document.forms["orderform"];
-    originalThinCrust = 0 ;
-    originalThinCrust=document.getElementById("thincrust");
-    if (thincrust.checked == true ){
-        originalThinCrust=100;
-    }
-        return originalThinCrust;       
-} 
+// // function originalThinCrust(){
+// //     form=document.forms["orderform"];
+// //     originalThinCrust = 0 ;
+// //     originalThinCrust=document.getElementById("thincrust");
+// //     if (thincrust.checked == true ){
+// //         originalThinCrust=100;
+// //     }
+// //         return originalThinCrust;       
+// // } 
 
-function panCrust(){
-    form= document.forms["orderform"];
-    panCrust=0;
-    pancrust=document.getElementById("pancrust");
-    if (pancrust.checked == true){
-         pancrust=150;
-     }
-        return panCrust;
-}
+// // function panCrust(){
+// //     form= document.forms["orderform"];
+// //     panCrust=0;
+// //     pancrust=document.getElementById("pancrust");
+// //     if (pancrust.checked == true){
+// //          pancrust=150;
+// //      }
+// //         return panCrust;
+// // }
 
-function glutenFree(){
-    form= document.forms["orderform"];
-    glutenFree=0;
-    glutenFree=document.getElementById("glutencrust");
-    if (glutencrust.checked == true ){
-        glutenFree=200;
-    }
-        return glutenFree;
-}
+// // function glutenFree(){
+// //     form= document.forms["orderform"];
+// //     glutenFree=0;
+// //     glutenFree=document.getElementById("glutencrust");
+// //     if (glutencrust.checked == true ){
+// //         glutenFree=200;
+// //     }
+// //         return glutenFree;
+// // }
 
-function cheesCrust(){
-    form= document.forms["orderform"];
-    cheesCrust=0;
-    cheesCrust=document.getElementById("cheescrust");
-    if (cheescrust.checked == true){
-        cheesCrust=250;
-    }
-        return cheesCrust;
-}
+// // function cheesCrust(){
+// //     form= document.forms["orderform"];
+// //     cheesCrust=0;
+// //     cheesCrust=document.getElementById("cheescrust");
+// //     if (cheescrust.checked == true){
+// //         cheesCrust=250;
+// //     }
+// //         return cheesCrust;
+// // }
 
-function extraChees(){
-    form= document.forms["orderform"];
-    extraChees=0;
-    extrachees=document.getElementById("chees");
-    if (chees.checked == true){
-        extraChees=200;
-    }
-        return extraChees;
-}
+// // function extraChees(){
+// //     form= document.forms["orderform"];
+// //     extraChees=0;
+// //     extrachees=document.getElementById("chees");
+// //     if (chees.checked == true){
+// //         extraChees=200;
+// //     }
+// //         return extraChees;
+// // }
 
-function pepperoni(){
-    form= document.forms["orderform"];
-    pepperoni=0;
-    pepperoni=document.getElementById("pepperoni");
-    if(pepperoni.checked == true){
-        pepperoni=200;
-    }
-        return pepperoni;
-}
+// // function pepperoni(){
+// //     form= document.forms["orderform"];
+// //     pepperoni=0;
+// //     pepperoni=document.getElementById("pepperoni");
+// //     if(pepperoni.checked == true){
+// //         pepperoni=200;
+// //     }
+// //         return pepperoni;
+// // }
 
-function extraTomato(){
-    form= document.forms["orderform"];
-    extraTomato=0;
-    extraTomato=document.getElementById("tomato");
-    if (tomato.chacked == true){
-        extraTomato=200;
-    }
-        return extraTomato;
-}
+// // function extraTomato(){
+// //     form= document.forms["orderform"];
+// //     extraTomato=0;
+// //     extraTomato=document.getElementById("tomato");
+// //     if (tomato.chacked == true){
+// //         extraTomato=200;
+// //     }
+// //         return extraTomato;
+// // }
 
-function mushroom(){
-    form= document.forms["orderform"];
-    mushroom=0;
-    mushroom=document.getElementById("mushroom");
-    if (mushroom.checked == true){
-        mushroom=200;
-    }
-        return mushroom;
-}
+// // function mushroom(){
+// //     form= document.forms["orderform"];
+// //     mushroom=0;
+// //     mushroom=document.getElementById("mushroom");
+// //     if (mushroom.checked == true){
+// //         mushroom=200;
+// //     }
+// //         return mushroom;
+// // }
 
-function mexicanCheese(){
-    form= document.forms["orderform"];
-    mexicanCheese=0;
-    mexicanCheese=document.getElementById("mexican");
-    if(mushroom.checked == true){
-        mexicanCheese=200;
-    }
-        return mexicanCheese;
-}
+// // function mexicanCheese(){
+// //     form= document.forms["orderform"];
+// //     mexicanCheese=0;
+// //     mexicanCheese=document.getElementById("mexican");
+// //     if(mushroom.checked == true){
+// //         mexicanCheese=200;
+// //     }
+// //         return mexicanCheese;
+// // }
 
-function sausage(){
-    form = document.forms["orderform"];
-    sausage=0;
-    sausage=document.getElementById("sausage");
-    if(sausage.checked == true){
-        sausage=200;
-    }
-    return sausage;
-}
+// // function sausage(){
+// //     form = document.forms["orderform"];
+// //     sausage=0;
+// //     sausage=document.getElementById("sausage");
+// //     if(sausage.checked == true){
+// //         sausage=200;
+// //     }
+// //     return sausage;
+// // }
 
-function bellPeppers(){
-    form= document.forms["orderform"];
-    bellPeppers=0;
-    bellPeppers=document.getElementById("bell");
-    if(bell.checked == true){
-        bellPeppers=200;
-    }
-        return bellPeppers;
-}
+// // function bellPeppers(){
+// //     form= document.forms["orderform"];
+// //     bellPeppers=0;
+// //     bellPeppers=document.getElementById("bell");
+// //     if(bell.checked == true){
+// //         bellPeppers=200;
+// //     }
+// //         return bellPeppers;
+// // }
 
-function avacadoSpecial(){
-    form= document.forms["orderform"];
-    avacadoSpecial=0;
-    avacadoSpecial=document.getElementById("avacado");
-    if(avacado.checked == true){
-        avacadoSpecial=200;
-    }
-    return avacadoSpecial;
-}
+// // function avacadoSpecial(){
+// //     form= document.forms["orderform"];
+// //     avacadoSpecial=0;
+// //     avacadoSpecial=document.getElementById("avacado");
+// //     if(avacado.checked == true){
+// //         avacadoSpecial=200;
+// //     }
+// //     return avacadoSpecial;
+// // }
 
-function calculatePrice(){
-    var totalPrice=(getFlavor() + originalThinCrust() + panCrust() + glutenFree() + cheesCrust() + extraChees() + pepperoni() +extraTomato() + mushroom() + mexicanCheese() + sausage() + bellPeppers() +avacadoSpecial())*getQuantity();
-    var myPizza = document.getElementById("totalprice");
-    myPizza.style.display="block";
-    myPizza.innerHTML = "The price is Ksh " + totalPrice;
-}
+// // function calculatePrice(){
+// //     var totalPrice=(getFlavor() + originalThinCrust() + panCrust() + glutenFree() + cheesCrust() + extraChees() + pepperoni() +extraTomato() + mushroom() + mexicanCheese() + sausage() + bellPeppers() +avacadoSpecial())*getQuantity();
+// //     var myPizza = document.getElementById("totalprice");
+// //     myPizza.style.display="block";
+// //     myPizza.innerHTML = "The price is Ksh " + totalPrice;
+// // }
 
-function getQuantity(){
-     form=document.form["orderform"];
-    pizzas=0;
-    var quantity=document.getElementById("quantity");
-    if(quantity.value !=""){
-        quantity=parseInt(quantity.value);
-    }
-   return quantity;
-}
+// // function getQuantity(){
+// //      form=document.form["orderform"];
+// //     pizzas=0;
+// //     var quantity=document.getElementById("quantity");
+// //     if(quantity.value !=""){
+// //         quantity=parseInt(quantity.value);
+// //     }
+// //    return quantity;
+// // }
 
 
 
